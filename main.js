b09d29f601e9e3725f28e890028585fd46b07385
Md. Shakib Mia <smdshakibmia2001@gmail.com>
2024-08-25 22:45:51 +0600

    till footer with responsiveness done

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs";
+
 document
   .querySelectorAll("button")
   .forEach((item) =>
@@ -23,3 +25,78 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  // direction: "vertical",
+  // loop: true,
+
+  // If we need pagination
+  // pagination: {
+  //   el: ".swiper-pagination",
+  // },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".slider-button-next",
+    prevEl: ".slider-button-prev",
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
+
+const swiper2 = new Swiper(".swiper2", {
+  // Optional parameters
+  // direction: "vertical",
+  loop: true,
+  autoplay: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  // navigation: {
+  //   nextEl: ".slider-button-next",
+  //   prevEl: ".slider-button-prev",
+  // },
+  slidesPerView: 1,
+  // breakpoints: {
+  //   640: {
+  //     slidesPerView: 1,
+  //     spaceBetween: 0,
+  //   },
+  //   768: {
+  //     slidesPerView: 1,
+  //   },
+  //   1024: {
+  //     slidesPerView: 1,
+  //   },
+  // },
+});
+
+const lenis = new Lenis();
+
+lenis.on("scroll", (e) => {
+  console.log(e);
+});
+
+function raf(time) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
